b23370b8a4895f8445f40260c5ca676f3dec9d69
Julien Moutte <julien@moutte.net>
2005-11-28 15:23:13 +0000

    Adding flumotion-windowsmedia.
    svn path=/flumotion-advanced/flumotion-windowsmedia/; revision=1598

---
+++ common/common.mk
@@ -0,0 +1,47 @@
+if HAVE_PYCHECKER
+check-local: pychecker
+endif
+
+test:
+	@make check -C flumotion/test
+
+check-docs:
+	@make check -C doc/reference
+
+coverage:
+	@trial --coverage coverage -R flumotion.test
+	@test ! -z "$(COVERAGE_MODULES)" ||				\
+	(echo Define COVERAGE_MODULES in your Makefile.am; exit 1)
+	@keep="";							\
+	for m in $(COVERAGE_MODULES); do				\
+		echo adding $$m;					\
+		keep="$$keep `ls _trial_temp/coverage/$$m*`";		\
+	done;								\
+	$(PYTHON) common/show-coverage.py $$keep
+
+fixme:
+	tools/fixme | less -R
+
+release: dist
+	make $(PACKAGE)-$(VERSION).tar.bz2.md5
+
+# generate md5 sum files
+%.md5: %
+	md5sum $< > $@
+
+# generate a sloc count
+sloc:
+	sloccount flumotion | grep "(SLOC)" | cut -d = -f 2
+
+.PHONY: test
+
+
+locale-uninstalled:
+	if test -d po; then \
+	cd po; \
+	make datadir=../$(top_builddir) install; \
+	fi
+
+locale-uninstalled-clean:
+	@-rm -rf _trial_temp
+	@-rm -rf $(top_builddir)/locale
