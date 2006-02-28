3ad94f1351b97708feb5d2b0bae565e1d7db81e6
Thomas Vander Stichele <thomas@apestaart.org>
2006-02-28 18:32:28 +0000

    removing common, try :externals instead
    svn path=/flumotion-advanced/flumotion-windowsmedia/; revision=2203

--- common/locale.mk
+++
@@ -1,3 +0,0 @@
-$(PROJECT).locale.xml: $(top_srcdir)/po/LINGUAS
-	$(PYTHON) $(top_srcdir)/common/gen-locale-xml.py \
-                $(GETTEXT_PACKAGE) $(PROJECT) `cat $(top_srcdir)/po/LINGUAS` > $@
