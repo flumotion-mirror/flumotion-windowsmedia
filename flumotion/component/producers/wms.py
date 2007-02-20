26357c8378cf253eb970b9ffa88fb046939c58ef
Michael Smith <msmith@fluendo.com>
2007-02-20 12:54:35 +0000

    * flumotion/component/producers/wms.py:           Next checkpoint.           Can now negotiate with WME, and dump something that looks like ASF           to a file
    svn path=/flumotion-advanced/flumotion-windowsmedia/trunk/; revision=5242

--- flumotion/component/producers/wms.py
+++ flumotion/component/producers/wms.py
@@ -23,6 +23,55 @@ from twisted.web import resource, server, http
 from flumotion.component import component
 from flumotion.common import log
 
+class WMSParser(log.Loggable):
+    def __init__(self):
+        self._dumpfile = open("/tmp/dump.asf", "w")
+        self._rawdumpfile = open("/tmp/dump.raw", "w")
+        self._packet_remaining = 0 
+        self._header_remaining = 4
+        self._header = ""
+
+    def dataReceived(self, data):
+        self._rawdumpfile.write(data)
+        length = len(data)
+        offset = 0
+        self.debug("Received %d byte buffer", length)
+        while offset < length:
+            rem = length - offset
+            if self._header_remaining:
+                c = min(rem, self._header_remaining)
+                self._header += data[offset:offset+c]
+                self._header_remaining -= c
+
+                if self._header_remaining == 0:
+                    self.debug("Header received, now parsing")
+                    # Now parse the 4-byte header...
+                    if self._header[0] != '$':
+                        # TODO: try and recover somehow...
+                        self.warning("Synchronisation error")
+
+                    if self._header[1] == 'H':
+                        self.debug("Received header packet")
+                    elif self._header[1] == 'D':
+                        self.debug("Received data packet")
+                    else:
+                        self.warning("Unknown packet type: %s", self._header[1])
+
+                    self._packet_remaining = \
+                        (ord(self._header[3]) << 8) | \
+                        (ord(self._header[2]))
+                    self.debug("Packet is %d bytes long", self._packet_remaining)
+                offset += c
+            else:
+                c = min(rem, self._packet_remaining)
+                self._dumpfile.write(data[offset:offset+c])
+                self._packet_remaining -= c
+                offset += c
+
+                if self._packet_remaining == 0:
+                    self._header = ""
+                    self._header_remaining = 4
+
 class DigestAuth(log.Loggable):
     logCategory = "digestauth"
 
@@ -57,7 +106,7 @@ class DigestAuth(log.Loggable):
     def _generateNonce(self):
         return self._generateRandomString(16)
 
-    def _generateWWWAuthenticateHeader(self, request, stale=False):
+    def generateWWWAuthenticateHeader(self, request, stale=False):
         res = 'Digest'
         while True:
             opaque = self._generateOpaque()
@@ -65,16 +114,18 @@ class DigestAuth(log.Loggable):
             if opaque not in self._outstanding:
                 nonce = self._generateNonce()
                 self._outstanding[opaque] = (nonce, timestamp)
+                self.debug("Set opaque '%s' -> nonce '%s'", opaque, nonce)
                 break
 
         params = {'realm':  (True, self._realm),
                   'qop':    (False, self._qop_type),
                   'nonce':  (True, nonce),
                   'opaque': (True, opaque),
-                  'algorithm': (False, self._algorithm)
+                  # We don't send algorithm, if we do WME fails (WTF?)
+                  #'algorithm': (False, self._algorithm)
                  }
-        if stale:
-            params['stale'] = (False, 'true')
+        #if stale:
+        #    params['stale'] = (False, 'true')
 
         # TODO: add 'domain', maybe 'algorithm'? Figure out
         # auth vs. auth-int.
@@ -163,9 +214,9 @@ class DigestAuth(log.Loggable):
         Attempt to authenticate a request.
         Returns an HTTP response code (which should be set on the response)
         """
-        if not hasHeader(request, "Authorization"):
+        if not request.hasHeader("Authorization"):
             self.debug("No auth header, sending unauthorized")
-            return http.UNAUTHORIZED
+            return (http.UNAUTHORIZED, False)
 
         self.debug("Has auth header, parsing")
 
@@ -173,14 +224,14 @@ class DigestAuth(log.Loggable):
         type, attrs = self._parseAuthHeader(authHeader)
         if type.lower() != 'digest':
             self.debug("Not digest auth, bad request")
-            return http.BAD_REQUEST
+            return (http.BAD_REQUEST, False)
 
         self.debug("Received attributes: %r", attrs)
         required = ['username', 'realm', 'nonce', 'opaque', 'uri', 'response']
         for r in required:
             if r not in attrs:
                 self.debug("Required attribute %s is missing", r)
-                return http.BAD_REQUEST
+                return (http.BAD_REQUEST, False)
 
         # 'qop' is optional, if sent then cnonce and nc are required.
         qop = False
@@ -188,7 +239,7 @@ class DigestAuth(log.Loggable):
             if attrs['qop'] != 'auth' or 'cnonce' not in attrs or \
                     'nc' not in attrs:
                 self.debug("qop is not auth or cnonce missing or nc missing")
-                return http.BAD_REQUEST
+                return (http.BAD_REQUEST, False)
             qop = True
             nccount = attrs['nc']
             cnonce = attrs['cnonce']
@@ -197,7 +248,7 @@ class DigestAuth(log.Loggable):
             if self._algorithm == 'md5-sess':
                 if 'cnonce' not in attrs:
                     self.debug("cnonce not present when md5-sess in use")
-                    return http.BAD_REQUEST
+                    return (http.BAD_REQUEST, False)
             nccount = None
             cnonce = None
             
@@ -216,17 +267,17 @@ class DigestAuth(log.Loggable):
 
         if username not in self._users:
             self.debug("Username not found in users db")
-            return http.UNAUTHORIZED
+            return (http.UNAUTHORIZED, False)
 
         password = self._users[username]
 
         if opaque not in self._outstanding:
             self.debug("opaque not in outstanding")
-            return http.UNAUTHORIZED
+            return (http.UNAUTHORIZED, True)
         (expectednonce, ts) = self._outstanding[opaque]
         if expectednonce != nonce:
             self.debug("nonce doesn't correspond to opaque")
-            return http.BAD_REQUEST
+            return (http.BAD_REQUEST, True) # TODO:Should this be True or False?
         # TODO: expire entries based on ts?
 
         expected = self._calculateRequestDigest(realm, nonce, qop, nccount, 
@@ -237,66 +288,135 @@ class DigestAuth(log.Loggable):
             response)
         if response != expected:
             self.debug("Password incorrect")
-            return http.UNAUTHORIZED
+            return (http.UNAUTHORIZED, False)
 
         # Success!
-        return http.OK
+        return (http.OK, False)
 
-def hasHeader(request, header):
-    return header.lower() in request.getAllHeaders()
-        
-class TestResource(resource.Resource, log.Loggable):
+class WMSRequest(server.Request, log.Loggable):
 
-    def __init__(self, realm):
-        resource.Resource.__init__(self)
-        self.isLeaf = True # Don't look any deeper
+    def __init__(self, *args, **kw):
+        server.Request.__init__(self, *args, **kw)
 
-        self._digester = DigestAuth(realm)
-        self._digester.addUser("user", "test")
+        self._streaming = False
+        self._wmsparser = None
 
-    def render_POST(self, request):
-        request.setHeader("Supported", 
+    def hasHeader(self, header):
+        return header.lower() in self.received_headers
+
+    def setStreaming(self):
+        self._streaming = True
+
+    def finish(self):
+        if not self._streaming:
+            server.Request.finish(self)
+
+    def process(self):
+        digester = self.channel.wmsfactory.digester
+        # Pretend to be Windows Media Server, otherwise WME won't connect.
+        # TODO: identify ourselves somewhere in here.
+        self.setHeader("Server", "Cougar/9.01.01.3814")
+        self.setHeader("Date", http.datetimeToString())
+        self.setHeader("Supported", 
             "com.microsoft.wm.srvppair, com.microsoft.wm.sswitch, " \
             "com.microsoft.wm.predstrm, com.microsoft.wm.fastcache, " \
             "com.microsoft.wm.startupprofile")
-        request.setHeader("Server", "Cougar/9.01.01.3814")
-
-        self.debug("Trying authentication")
-        code = self._digester.authenticate(request):
-
-        if code >= 400:
-            self.debug("Authentication failed")
-            request.setResponseCode(code)
-            if code == 401:
-                # TODO: This nasty hack is needed because setHeader() mangles
-                # case, which WMEncoder doesn't cope with
-                request.headers["WWW-Authenticate"] = \
-                    self._generateWWWAuthenticateHeader(request)
-            return ""
+        self.setHeader("Content-Length", 0)
+        self.setHeader("Pragma", "no-cache,timeout=60000")
 
         pushId = 0
-        if hasHeader(request, "Cookie"):
-            cookieKV = request.getHeader("Cookie")
+        if self.hasHeader("Cookie"):
+            cookieKV = self.getHeader("Cookie")
             cookieKey, cookieVal = cookieKV.split('=')
             if cookieKey == 'push-id':
                 pushId = int(cookieVal)
         if not pushId:
-            pushId = 213792184 # Some random number. TODO!
+            pushId = 231271312 # Some random number. TODO!
 
-        ctype = request.getHeader("content-type")
+        if not pushId: # TODO: Check for validity?
+            self.debug("Trying authentication")
+            (code, stale) = digester.authenticate(self)
+        else:
+            code = 200
+
+        if code >= 400:
+            self.debug("Authentication failed")
+            self.setResponseCode(code)
+            if code == 401:
+                # TODO: This nasty hack is needed because setHeader() mangles
+                # case, which WMEncoder doesn't cope with
+                class HackString(str):
+                    def capitalize(self):
+                        return self
+
+                self.headers[HackString("WWW-Authenticate")] = \
+                    digester.generateWWWAuthenticateHeader(self, stale)
+                if pushId:
+                    self.headers[HackString("Set-Cookie")] = \
+                        "push-id=%d" % pushId
+            self.finish()
+            return
+
+        ctype = self.getHeader("content-type")
         if ctype == 'application/x-wms-pushsetup':
             if pushId:
-                request.headers["Set-Cookie"] = "push-id=%d" % pushId
-            request.setResponseCode(http.NO_CONTENT)
-            return ""
+                self.headers["Set-Cookie"] = "push-id=%d" % pushId
+            self.setResponseCode(http.NO_CONTENT)
+            self.finish()
+            return
         elif ctype == 'application/x-wms-pushstart':
             self.debug("Got pushstart!")
-            # Need to make this work: TODO!
-            request.content = open("/tmp/out.dump", "w")
-            return ""
+            self._wmsparser = WMSParser()
+            self.finish()
+            return
         else:
             self.debug("Unknown content-type: %s", ctype)
-            return ""
+            self.finish()
+            return
+
+    def dataReceived(self, data):
+        if not self._wmsparser:
+            self.warning("Receiving streaming data without a pushstart request")
+            return
+
+        self._wmsparser.dataReceived(data)
+
+class WMSChannel(http.HTTPChannel, log.Loggable):
+
+    def __init__(self):
+        http.HTTPChannel.__init__(self)
+
+        self._streaming_request = None
+
+    def rawDataReceived(self, data):
+        # Windows Media Encoder sends this content-length. Use this as a trigger
+        # to switch to our streaming-POST interface
+        if self.length == 2147483647 or self._streaming_request:
+            if not self._streaming_request:
+                self.debug("Got max-length request, switching to streaming POST")
+                self._streaming_request = self.requests[-1]
+                self._streaming_request.setStreaming()
+                self.allContentReceived()
+            self.debug("Data received for streaming post request")
+            self._streaming_request.dataReceived(data)
+        else:
+            self.debug("Raw data received for non-streaming request")
+            http.HTTPChannel.rawDataReceived(self, data)
+
+class WMSFactory(http.HTTPFactory):
+    protocol = WMSChannel
+    requestFactory = WMSRequest
+
+    def __init__(self, auth):
+        http.HTTPFactory.__init__(self)
+
+        self.digester = auth
+
+    def buildProtocol(self, addr):
+        channel = http.HTTPFactory.buildProtocol(self, addr)
+        channel.requestFactory = self.requestFactory
+        channel.wmsfactory = self
+        return channel
 
 class WindowsMediaServer(component.BaseComponent):
     """
@@ -306,7 +426,9 @@ class WindowsMediaServer(component.BaseComponent):
 
     def do_start(self, *args, **kwargs):
         # TODO: Write a real component!
-        resource = TestResource("Flumotion Streaming Server WMS Component")
-        reactor.listenTCP(8888, server.Site(resource=resource))
+        digester = DigestAuth("Flumotion Streaming Server WMS Component")
+        digester.addUser("user", "test")
+        reactor.listenTCP(8888, WMSFactory(digester))
 
         return defer.succeed(None)
+
