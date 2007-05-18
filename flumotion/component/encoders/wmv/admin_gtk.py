8a607304a7ad4a0c743b8184b92fc10b3d37cb7d
Julien Moutte <julien@moutte.net>
2007-05-18 16:21:55 +0000

    2007-05-18  Julien MOUTTE  <julien@moutte.net>
            * flumotion/component/encoders/wmv/admin_gtk.py:
            * flumotion/component/encoders/wmv/wmv.glade:
            * flumotion/component/encoders/wmv/wmv.py:
            * flumotion/component/encoders/wmv/wmv.xml: Make the WMV
            encoder autodiscover available GStreamer plug-ins and report
            errors to the admin UI. Implement a GTK UI for knowing which
            encoder is in use and additional informations about it's
            configuration
    
    svn path=/flumotion-advanced/flumotion-windowsmedia/trunk/; revision=6698

---
+++ flumotion/component/encoders/wmv/admin_gtk.py
@@ -0,0 +1,68 @@
+# -*- Mode: Python -*-
+# vi:si:et:sw=4:sts=4:ts=4
+#
+# Flumotion - a streaming media server
+# Copyright (C) 2004,2005,2006,2007 Fluendo, S.A. (www.fluendo.com).
+# All rights reserved.
+
+# Licensees having purchased or holding a valid Flumotion Advanced
+# Streaming Server license may use this file in accordance with the
+# Flumotion Advanced Streaming Server Commercial License Agreement.
+# See "LICENSE.Flumotion" in the source distribution for more information.
+
+# Headers in this file shall remain intact.
+
+from gettext import gettext as _
+
+from flumotion.component.base import admin_gtk
+
+class WMVEncoderAdminGtkNode(admin_gtk.BaseAdminGtkNode):
+    logCategory = 'wmv'
+    glade_file = 'flumotion/component/encoders/wmv/wmv.glade'
+    uiStateHandlers = None
+
+    def haveWidgetTree(self):
+        self.widget = self.wtree.get_widget('widget-wmv')
+        self._encoder = self.wtree.get_widget('value-encoder')
+        self._version = self.wtree.get_widget('value-version')
+        self._bitrate = self.wtree.get_widget('value-bitrate')
+        self._complexity = self.wtree.get_widget('value-complexity')
+
+    def setUIState(self, state):
+        admin_gtk.BaseAdminGtkNode.setUIState(self, state)
+        if not self.uiStateHandlers:
+            self.uiStateHandlers = {'encoder': self.encoderSet,
+                                    'version': self.versionSet,
+                                    'bitrate': self.bitrateSet,
+                                    'complexity': self.complexitySet }
+        for k, handler in self.uiStateHandlers.items():
+            handler(state.get(k))
+                                    
+    def stateSet(self, state, key, value):
+        handler = self.uiStateHandlers.get(key, None)
+        if handler:
+            handler(value)
+        
+    def encoderSet(self, encoder):
+    	if self._encoder:
+            self._encoder.set_text(str(encoder))
+
+    def versionSet(self, version):
+        if self._version:
+            self._version.set_text(str(version))
+        
+    def bitrateSet(self, bitrate):
+    	if self._bitrate:
+            self._bitrate.set_text(str(bitrate))
+
+    def complexitySet(self, complexity):
+        if self._complexity:
+            self._complexity.set_text(str(complexity))
+
+class WMVEncoderAdminGtk(admin_gtk.BaseAdminGtk):
+    def setup(self):
+        wmvnode = WMVEncoderAdminGtkNode(self.state, self.admin,
+                                         title="Encoder")
+        self.nodes['Encoder'] = wmvnode
+
+        return admin_gtk.BaseAdminGtk.setup(self)
--- flumotion/component/encoders/wmv/wmv.py
+++ flumotion/component/encoders/wmv/wmv.py
@@ -2,7 +2,7 @@
 # vi:si:et:sw=4:sts=4:ts=4
 #
 # Flumotion - a streaming media server
-# Copyright (C) 2004,2005,2006 Fluendo, S.L. (www.fluendo.com).
+# Copyright (C) 2004,2005,2006,2007 Fluendo, S.A. (www.fluendo.com).
 # All rights reserved.
 
 # Licensees having purchased or holding a valid Flumotion Advanced
@@ -12,30 +12,85 @@
 
 # Headers in this file shall remain intact.
 
+from twisted.internet import defer
 from flumotion.component import feedcomponent
+from flumotion.common import messages, gstreamer, common
+
+from flumotion.common.messages import N_
+T_ = messages.gettexter('flumotion')
 
 class WMVEncoder(feedcomponent.ParseLaunchComponent):
     checkTimestamp = True
     checkOffset = True
+    wmvEncoder = None
+    wmvVersion = None
+
+    def init(self):
+        self.uiState.addKey('encoder', 0)
+        self.uiState.addKey('version', 0)
+        self.uiState.addKey('bitrate', 0)
+        self.uiState.addKey('complexity', 0)
 
     def do_check(self):
-        self.debug('running pitfdll check')
-        from flumotion.worker.checks import check
-        d = check.checkPlugin('pitfdll', 'gst-pitfdll')
-        def cb(result):
-            for m in result.messages:
+        self.debug('running Windows Media Video encoder check.')
+
+        version = self.config['properties'].get('version', 3)
+
+        # For WMV2 we need dmoenc_wmvdmoe2v2.
+        if version == 2:
+            if gstreamer.element_factory_exists('dmoenc_wmvdmoe2v2'):
+                self.wmvEncoder = 'dmoenc_wmvdmoe2v2'
+                self.wmvVersion = gstreamer.get_plugin_version('pitfdll')
+            else:
+                version  = gstreamer.get_plugin_version('pitfdll')
+                if not version:
+                    self.warning('could not find pitfdll.')
+                    m = messages.Error(T_( 
+                        N_("This host is missing the 'gst-pitfdll' GStreamer plug-in.\n")))
+                else:
+                    self.warning('could not find dmoenc_wmvdmoe2v2, probably missing DLL, or old registry.')
+                    m = messages.Error(T_(
+                        N_("This host is missing the Windows encoder DLL.\n")))
+                self.wmvEncoder = None
                 self.addMessage(m)
-        d.addCallback(cb)
-        return d
+        else:
+            # First look for Fluendo WMV encoder.
+            if gstreamer.element_factory_exists('fluwmvenc'):
+                self.debug('found fluwmvenc, using it.')
+                self.wmvEncoder = 'fluwmvenc'
+                self.wmvVersion = gstreamer.get_plugin_version('fluwmvenc')
+            elif gstreamer.element_factory_exists('dmoenc_wmvdmoe2v3'):
+                self.debug('could not find fluwmvenc, found dmoenc_wmvdmoe2v3.')
+                self.wmvEncoder = 'dmoenc_wmvdmoe2v3'
+                self.wmvVersion = gstreamer.get_plugin_version('pitfdll')
+            else:
+                self.warning('could not find any WMV encoder.')
+                m = messages.Error(T_(
+                    N_("This host is missing the WMV encoder plug-ins.\n")))
+                self.wmvEncoder = None
+                self.addMessage(m)
+
+        self.uiState.set('encoder', self.wmvEncoder)
+        self.uiState.set('version', self.wmvVersion)
+
+        return defer.succeed(None)
 
     def get_pipeline_string(self, properties):
-        wmv_encoder = 'dmoenc_wmvdmoe2v3'
-        if properties.has_key('version'):
-            if properties['version'] == 2:
-                wmv_encoder = 'dmoenc_wmvdmoe2v2'
-        return "ffmpegcolorspace ! %s name=encoder" % wmv_encoder
+        return "ffmpegcolorspace ! %s name=encoder" % self.wmvEncoder
 
     def configure_pipeline(self, pipeline, properties):
         element = pipeline.get_by_name('encoder')
         if properties.has_key('bitrate'):
             element.set_property('bitrate', properties['bitrate'])
+        if properties.has_key('complexity'):
+            element.set_property('complexity', properties['complexity'])
+        if properties.has_key('sharpness'):
+            element.set_property('sharpness', properties['sharpness'])
+        if properties.has_key('key-frame-distance'):
+            element.set_property('key-frame-distance', properties['key-frame-distance'])
+        if properties.has_key('buffer-delay'):
+            element.set_property('buffer-delay', properties['buffer-delay'])
+
+        self.uiState.set('complexity', element.get_property('complexity'))
+        self.uiState.set('bitrate', common.formatStorage(element.get_property('bitrate')) + 'bit/s')
+
